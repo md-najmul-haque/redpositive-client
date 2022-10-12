61ec7183f4d97bae4ec53bf559468902a3f464b4
Md Najmul Haque <najmulhaque008@gmail.com>
2022-10-12 17:38:13 +0600

    add new data modal added

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import Form from './Components/Form/Form';
 import Table from './Components/Table/Table';
+import InputForm from './Components/InputForm/InputForm';
 
 function App() {
   return (
     <div className="container mx-auto">
       <Navbar />
+      <InputForm />
       <Form />
       <Table />
     </div>
---
+++ src/Components/InputForm/InputForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const InputForm = () => {
+    return (
+        <div>
+            <label htmlFor="my-modal-6" className="btn modal-button">Add New Data</label>
+        </div>
+    );
+};
+
+export default InputForm;
